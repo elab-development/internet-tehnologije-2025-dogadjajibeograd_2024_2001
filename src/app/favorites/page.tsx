4b5b187969bdd900efd141bcf84f9a63d188d80d
md20242001 <md20242001@student.fon.bg.ac.rs>
2026-08-24 07:22:01 +0200

    add favorites, profile and upcoming events pages

---
+++ src/app/favorites/page.tsx
@@ -0,0 +1,5 @@
+export default function UpcomingHomepage(){
+    return(
+        <p>Strana u izradi</p>
+    );
+}
---
+++ src/app/profile/page.tsx
@@ -0,0 +1,5 @@
+import ProfailForm from "@/components/ProfaileFrom";
+
+export default async function ProfilePage(){
+   return <ProfailForm/>
+}
